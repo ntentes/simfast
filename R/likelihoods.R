67db887c71d99ee22a26cd65694ebc5916e78c6a
Tino Ntentes <tino65@gmail.com>
2020-05-07 20:24:09 -0400

    added gamma and poisson likelihoods

--- R/likelihoods.R
+++ R/likelihoods.R
@@ -1,35 +1,73 @@
 
 #' Log Likelihood Function for Gaussian Family (Internal)
 #'
-#' @param mle vector of maximum likelihood estimator
+#' @param mle vector, maximum likelihood estimator
 #' @param y vector of numeric response values
 #' @param nn vector of positive integer weights
 #'
 #' @return numeric value of log likelihood function
 #'
 #'
-find.loglik.gaussian	<-	function(mle, y, nn){
-  ll	<-	sum(nn*dnorm(y, mean = mle, log = TRUE))
+find.loglik.gaussian	<-	function(mle, y, nn) {
+  ll	<-	sum(nn * dnorm(y, mean = mle, log = TRUE))
   return(ll)
 }
 
 
 #' Log Likelihood Function for Binomial Family (Internal)
 #'
-#' @param mle vector of maximum likelihood estimator
+#' @param mle vector, maximum likelihood estimator
 #' @param y vector of binary response values
 #' @param nn vector of positive integer weights
 #'
 #' @return numeric value of log likelihood function
 #'
 #'
-find.loglik.binomial	<-	function(mle, y, nn){
+find.loglik.binomial	<-	function(mle, y, nn) {
   newy <- y * nn
   if (!isTRUE(all(newy == floor(newy)))) {
     warning("y * weights are not all integers, responses will be rounded.")
     newy <- round(newy)
   }
   ll <- sum(dbinom(x = newy, size = nn, prob = mle, log = TRUE))
+  return(ll)
+}
+
+
+#' Log Likelihood Function for Poisson Family (Internal)
+#'
+#' @param mle vector, maximum likelihood estimator
+#' @param y vector of positive integer response values
+#' @param nn vector of positive integer weights
+#'
+#' @return numeric value of log likelihood function
+#'
+#'
+find.loglik.poisson   <-  function(mle, y, nn) {
+  if (!isTRUE(all(y == floor(y)))) {
+    warning("Response values are not all integers, responses will be rounded.")
+    newy <- round(y)
+  }
+  ll <- sum(nn * dpois(x = newy, lambda = mle, log = TRUE))
+  return(ll)
+}
+
+
+#' Log Likelihood Function for Gamma with Unknown Shape Family (Internal)
+#'
+#' @param mle vector, maximum likelihood estimator
+#' @param y vector of positive response values
+#' @param nn vector of positive integer weights
+#'
+#' @return numeric value of log likelihood function
+#'
+#'
+find.loglik.gamma   <-  function(mle, y, nn) {
+  if (!isTRUE(all(y == abs(y)))) {
+    stop("Response values are not all positive, regression cannot contiue.")
+  }
+  ll <- sum(nn * dgamma(x = y, shape = mle, log = TRUE))
+  return(ll)
 }
 
 
@@ -39,18 +77,23 @@ find.loglik.binomial	<-	function(mle, y, nn){
 
 #' Selects and Runs Likelihood for Given Distribution Family (Internal)
 #'
-#' @param mle vector of maximum likelihood estimator
+#' @param mle vector, maximum likelihood estimator
 #' @param y vector of numeric response values
 #' @param nn vector of positive integer weights
 #' @param family character string matching the error distribution or link function
 #'
 #' @return numeric value of log likelihood function
 #'
-find.loglik <- function(mle, y, nn, family){
-  if (family == 'gaussian'){
+find.loglik <- function(mle, y, nn, family) {
+  if (family %in% c('gaussian', 'gaussianlog')) {
     find.loglik.gaussian(mle, y, nn)
-  } else if (family %in% c('logit', 'binomial')){
-    find.loglik.logit(mle, y, nn)
+  } else if (family %in% c('logit', 'probit', 'cloglog', 'cauchit',
+                           'binomlog', 'binomial')) {
+    find.loglik.binomial(mle, y, nn)
+  } else if (family %in% c('poisson', 'poissonlog', 'poissonident')) {
+    find.loglik.poisson(mle, y, nn)
+  } else if (family %in% c('gamma', 'gammainv', 'gammaident', 'gammalog')) {
+    find.loglik.gamma(mle, y, nn)
   } else {
     stop("No likelihood defined: please ensure valid family is selected.")
   }
