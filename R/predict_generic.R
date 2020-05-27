3c2f8da57033dce6bfc8a7028a75bb864c10d75a
Tino Ntentes <tino65@gmail.com>
2020-05-26 22:51:53 -0400

    changed predict to only output response values

--- R/predict_generic.R
+++ R/predict_generic.R
@@ -2,10 +2,6 @@
 #'
 #' @param object an object of class \code{simfast}
 #' @param data optional numeric matrix of new data
-#' @param type character string specifying type of prediction, takes
-#'     value \code{'link'} by default for predictions on the scale of
-#'     the linear predictors, and takes \code{'response'} for predicted
-#'     response values.
 #' @param rule integer that describes how to handle predictor values
 #'     outside the range of the original data. By default, the value
 #'     is \code{1}, which uses linear extrapolation to estimate outside
@@ -13,14 +9,13 @@
 #'     the closest edge point.
 #' @param fn interpolation function passed from \code{predict.simfast}
 #' @param interp extrapolation function passed from \code{predict.simfast}
-#' @param link link function passed from \code{predict.simfast}
 #'
 #' @return a numeric vector
 #'
 #' @noRd
 #' @keywords internal
 #'
-mat_pred <- function(object, newdata, type, rule, fn, interp, link){
+mat_pred <- function(object, newdata, rule, fn, interp){
   newdata  <- as.matrix(newdata)
   newdata  <- stats::na.omit(newdata)
   rwnms <- rownames(newdata)
@@ -44,27 +39,13 @@ mat_pred <- function(object, newdata, type, rule, fn, interp, link){
     stop("'newdata' must be a numeric matrix or object$model must contain a model.frame")
   } else {
     newyhat <- fn(newivs)
-    if (type == 'link') {
-      if (rule == 2) {
-        names(newyhat) <- rwnms
-        return(newyhat)
-      } else {
-
-        newyhat[oob] <- interp(newivs)[oob]
-        names(newyhat) <- rwnms
-        return(newyhat)
-      }
+    if (rule == 2) {
+      names(newyhat) <- rwnms
+      return(newyhat)
     } else {
-      if (rule == 2) {
-        newyhat <- link(newyhat)
-        names(newyhat) <- rwnms
-        return(newyhat)
-      } else {
-        newyhat[oob] <- interp(newivs)[oob]
-        newyhat <- link(newyhat)
-        names(newyhat) <- rwnms
-        return(newyhat)
-      }
+      newyhat[oob] <- interp(newivs)[oob]
+      names(newyhat) <- rwnms
+      return(newyhat)
     }
   }
 }
@@ -76,6 +57,8 @@ mat_pred <- function(object, newdata, type, rule, fn, interp, link){
 #'
 #' Predict values of a \code{simfast} object with a data frame or a matrix.
 #' Can also be called by running \code{\link{predict}} on \code{simfast} object.
+#' Note that \code{simfast} fits models on the response level, so only
+#' 'response' values are predicted.
 #'
 #' @param object an object of class \code{simfast}
 #' @param newdata optional numeric matrix or data frame of new data. If
@@ -85,10 +68,6 @@ mat_pred <- function(object, newdata, type, rule, fn, interp, link){
 #'     value). New numeric matrix or data frame must match the structure of the
 #'     original data (unused columns in data frames need not be included). Note
 #'     that rows with \code{NA} values will be removed.
-#' @param type character string specifying type of prediction, takes
-#'     value \code{'link'} by default for predictions on the scale of
-#'     the linear predictors, and takes \code{'response'} for predicted
-#'     response values.
 #' @param rule integer that describes how to handle predictor values
 #'     outside the range of the original data. By default, the value
 #'     is \code{1}, which uses linear extrapolation to estimate outside
@@ -108,39 +87,36 @@ mat_pred <- function(object, newdata, type, rule, fn, interp, link){
 #'
 #' # See the example provided in the \code{\link{simfast}} documentation.
 #'
-predict.simfast <- function(object, newdata, type = 'link', rule = 1, ...){
+predict.simfast <- function(object, newdata, rule = 1, ...){
   family <- object$family
   linkinv <- family$linkinv
   linkfun <- family$linkfun
   if (missing(newdata)){
-    if (type == 'response'){
-      return(object$yhat)
-    } else {
-      etahat <- linkfun(object$yhat)
-      return(etahat)
-    }
+    return(object$yhat)
   }
   oldy <- object$yhat
-  osy <- oldy
-  linkind <- sapply(osy, family$validmu)
-  osy <- linkfun(osy[linkind])
-  predfun <- stats::approxfun(x = object$indexvals[linkind], y = osy,
+  predfun <- stats::approxfun(x = object$indexvals, y = oldy,
                               method = "linear", rule = 2, ties = mean)
   interp  <- function(ind) {
-    x1 <- min(object$indexvals[linkind])
-    x2 <- max(object$indexvals[linkind])
-    y1 <- ifelse(family[[1]] == 'Gamma',
-                 max(min(osy), 0), min(osy))
-    y2 <- max(osy)
+    x1 <- min(object$indexvals)
+    x2 <- max(object$indexvals)
+    y1 <- min(oldy)
+    y2 <- max(oldy)
     m  <- (y2-y1)/(x2-x1)
     b  <- y2 - x2*m
     ylink <- m * ind + b
+    if (family[[1]] %in% c('Gamma', 'poisson')) {
+      ylink[ylink < 0] <- 0
+    } else if (family[[1]] == 'binomial') {
+      ylink[ylink < 0] <- 0
+      ylink[ylink > 1] <- 1
+    }
     return(ylink)
   }
   if (is.null(object$model)) {
     if (is.matrix(newdata)) {
-      predvec <- mat_pred(object = object, newdata = newdata, type = type,
-                          rule = rule, fn = predfun, interp = interp, link = linkinv)
+      predvec <- mat_pred(object = object, newdata = newdata, rule = rule,
+                          fn = predfun, interp = interp)
       return(predvec)
     } else {
       newdata <- as.matrix(newdata)
@@ -148,8 +124,8 @@ predict.simfast <- function(object, newdata, type = 'link', rule = 1, ...){
         stop("If model = NULL in the simfast object, then a numeric matrix
            must be provided, not a data frame.")
       } else {
-        predvec <- mat_pred(object = object, newdata = newdata, type = type,
-                            rule = rule, fn = predfun, interp = interp, link = linkinv)
+        predvec <- mat_pred(object = object, newdata = newdata, rule = rule,
+                            fn = predfun, interp = interp)
         return(predvec)
       }
     }
@@ -168,33 +144,34 @@ predict.simfast <- function(object, newdata, type = 'link', rule = 1, ...){
       newoffset <- stats::model.offset(newmf)
       oldy <- object$yhat
       osy <- oldy
-      linkind <- sapply(osy, family$validmu) # vals in range of link
-      osy <- linkfun(osy[linkind]) - object$offset[linkind]
-      pfos <- stats::approxfun(x = object$indexvals[linkind], y = osy,
+      osy <- linkfun(osy) - object$offset
+      osy <- linkinv(osy)
+      pfos <- stats::approxfun(x = object$indexvals, y = osy,
                                method = "linear", rule = 2, ties = mean)
       intos  <- function(ind) {
-        x1 <- min(object$indexvals[linkind])
-        x2 <- max(object$indexvals[linkind])
+        x1 <- min(object$indexvals)
+        x2 <- max(object$indexvals)
         y1 <- min(osy)
         y2 <- max(osy)
         m  <- (y2-y1)/(x2-x1)
         b  <- y2 - x2*m
-        yhatint <- m * ind + b
-        return(yhatint)
+        ylink <- m * ind + b
+        if (family[[1]] %in% c('Gamma', 'poisson')) {
+          ylink[ylink < 0] <- 0
+        } else if (family[[1]] == 'binomial') {
+          ylink[ylink < 0] <- 0
+          ylink[ylink > 1] <- 1
+        }
+        return(ylink)
       }
-      predvec <- mat_pred(object = object, newdata = newmm, type = 'link', rule = rule,
-                          fn = pfos, interp = intos, link = linkinv)
-      yhatos <- predvec + newoffset
-      if (type == 'link') {
-        predvec <- yhatos
-        return(predvec)
-      } else {
-        predvec <- linkinv(yhatos)
-        return(predvec)
-      }
+      predvec <- mat_pred(object = object, newdata = newmm, rule = rule,
+                          fn = pfos, interp = intos)
+      yhatos <- linkfun(predvec) + newoffset
+      predvec <- linkinv(yhatos)
+      return(predvec)
     } else {
-      predvec <- mat_pred(object = object, newdata = newmm, type = type, rule = rule,
-                        fn = predfun, interp = interp, link = linkinv)
+      predvec <- mat_pred(object = object, newdata = newmm, rule = rule,
+                          fn = predfun, interp = interp)
       return(predvec)
     }
   }
